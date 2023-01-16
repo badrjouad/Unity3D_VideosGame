8950a6bbcb5f3f6a860370ad78a25e6f19e9ac63
jeremythorouss <jeremy@thorouss.xyz>
2023-01-16 15:52:02 +0100

    version qui marche pas trop

--- Assets/Scripts/MyBehaviorTrees/ConditionIsTargetAlive.cs
+++ Assets/Scripts/MyBehaviorTrees/ConditionIsTargetAlive.cs
@@ -18,4 +18,17 @@ public class ConditionIsTargetAlive : Decorator
         }
         return TaskStatus.Failure;
 	}
+	/*public override bool CanExecute()
+    {
+            // CanExecute is called when checking the condition within a while loop so it will be called at least twice. Ensure the conditional task is checked only once
+            if (checkConditionalTask) {
+                checkConditionalTask = false;
+                OnUpdate();
+            }
+
+            if (conditionalTaskFailed) {
+                return false;
+            }
+            return executionStatus == TaskStatus.Inactive || executionStatus == TaskStatus.Running;
+    }*/
 }
